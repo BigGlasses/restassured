85bcc3c572fbf0280bea110c15c26e10a9d37575
BrandonNicholas Roberts <roberb1@mcmaster.ca>
2017-11-16 22:20:33 -0500

    RestChain Class

--- src/code/RestChain.js
+++ src/code/RestChain.js
@@ -1,26 +1,51 @@
-import * as Constants from 'Constants.js';
+// import * as Constants from 'Constants.js';
 
-
-class TestStub {
+/*
+  Class for running tests in order.
+*/
+class RestChain {
+  /*
+	Creast a RestChain.
+  */
   constructor(data) {
   	this.label = "RestChain";
   	this.reststublist = [];
   	this.identifier = "";
+  	if (data != null){
+  		this.identifier = data.identifier;
+  		this.reststublist = data.reststublist;
+  		this.label = data.label;
+  	}
   }
 
+  /*
+	Adds a test identifier to the TestChain at the end.
+  */
   addTest(identifier){
-
+  	this.reststublist.add(identifier);
   }
 
+  /*
+	Removes a test identifier from the TestChain.
+  */
   removeTest(identifier){
-
+  	if (identifier in this.reststublist)
+  			this.reststublist.splice(array.indexOf(identifier), 1);
   }
 
-  moveTest(identifier, location){
+  /*
 
+  */
+  moveTest(identifier, location){
+  	if (identifier in this.reststublist){
+	  	this.removeTest(identifier);
+	  	this.reststublist.splice(location, 0, identifier);
+	}
   }	
 
+  /*
 
+  */
   toJson(){
   	data = {};
 	data.label = this.label;
